92e2ff183a93ef4a9815d74cd6765ab4e6fe019f
L <EBogatyrevaPK@yandex.ru>
2022-07-03 20:36:17 +1200

    Посмотрите пожалуйста я Вас правильно поняла?

--- src/main/java/ru/hogwarts/school1/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school1/controller/InfoController.java
@@ -3,6 +3,7 @@ package ru.hogwarts.school1.controller;
 import liquibase.pro.packaged.S;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
